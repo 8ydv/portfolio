55876f97dfd31e830523afe6d81fb67efd4a32ff
Mohammed <116570281+8ydv@users.noreply.github.com>
2026-05-08 10:36:58 +0300

    Adjust left position of view counter widget for mobile

--- js/view-counter.js
+++ js/view-counter.js
@@ -183,7 +183,7 @@ const ViewCounter = (() => {
     @media (max-width: 768px) {
   .vc-widget {
     bottom: 20px;
-    left: 5px;
+    left: 50px;
     transform: translateY(12px);
   }
   .vc-widget.vc-show {
